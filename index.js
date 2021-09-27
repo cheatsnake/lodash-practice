9823a9a9c75f15c38e9d6ec67650e5c14ea55019
Yury <yura.sviderko@live.com>
2021-09-27 20:48:03 +0300

    chunk

---
+++ index.js
@@ -0,0 +1,35 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+var _ = require('./lodash.js');
+//chunk
+function chunk(arr, size) {
+    if (size >= arr.length) {
+        return [arr];
+    }
+    else if (size <= 0) {
+        return [];
+    }
+    else {
+        var res = [];
+        while (arr.length > size) {
+            var a = [];
+            for (var i = 0; i < size; i++) {
+                a.push(arr.shift());
+            }
+            res.push(a);
+        }
+        if (arr.length && arr.length <= size) {
+            return __spreadArray(__spreadArray([], res, true), [arr], false);
+        }
+        return res;
+    }
+}
+console.log(chunk(['a', 'b', 'c', 'd', 'e', 'f'], 2));
+console.log(_.chunk(['a', 'b', 'c', 'd', 'e', 'f'], 2));
---
+++ index.ts
@@ -0,0 +1,27 @@
+const _ = require('./lodash.js'); //need for tests
+
+//chunk
+
+function chunk(arr: Array<any>, size: number): Array<any> {
+	if (size >= arr.length) {
+		return [arr];
+	} else if (size <= 0) {
+		return [];
+	} else {
+		let res = [];
+		while (arr.length > size) {
+			let a = [];		
+			for(let i = 0; i < size; i++) {
+				a.push(arr.shift());
+			}
+			res.push(a);
+		}
+		if (arr.length && arr.length <= size) {
+			return [...res, arr];
+		} 
+		return res;
+	}
+}
+
+// console.log(chunk(['a', 'b', 'c', 'd', 'e', 'f'], 2))
+// console.log(_.chunk(['a', 'b', 'c', 'd', 'e', 'f'], 2))
